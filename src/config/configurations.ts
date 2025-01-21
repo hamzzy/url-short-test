9ee116d398163960f2d8387e2e904ae7c99cc1d7
hamzzy <hamat.ibrahim3@gmail.com>
2025-01-21 08:45:07 +0100

    first commit

---
+++ src/config/configurations.ts
@@ -0,0 +1,14 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  redis: {
+    urls: (process.env.REDIS_URLS || 'redis://127.0.0.1:6379').split(','),
+  },
+  base_url: process.env.BASE_URL || 'http://localhost:3000',
+  cache_enabled: process.env.CACHE_ENABLED === 'true',
+  l1_cache_size: parseInt(process.env.L1_CACHE_SIZE, 10) || 1000,
+  rate_limit_window_sec: parseInt(process.env.RATE_LIMIT_WINDOW_SEC, 10) || 60,
+  rate_limit_max_requests:
+    parseInt(process.env.RATE_LIMIT_MAX_REQUESTS, 10) || 100,
+  cleanup_interval_min: parseInt(process.env.CLEANUP_INTERVAL_MIN, 10) || 60,
+  virtual_nodes: parseInt(process.env.VIRTUAL_NODES, 10) || 10,
+});
